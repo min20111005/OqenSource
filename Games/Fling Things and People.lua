c2e6b90cc7d43689e62e6b7c808c56b066d85db5
min20111005 <min20111005@iCloud.com>
2025-12-09 21:55:38 +0900

    Fix indentation and formatting in color update

--- Games/Fling Things and People.lua
+++ Games/Fling Things and People.lua
@@ -9,12 +9,12 @@ local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
 
 ReplicatedStorage.DataEvents.UpdateLineColorsEvent:FireServer(ColorSequence.new({
-      ColorSequenceKeypoint.new(0, Color3.fromRGB(117,189,33));
-      ColorSequenceKeypoint.new(1, Color3.fromRGB(255,199,40));
-      ColorSequenceKeypoint.new(2, Color3.fromRGB(255,102,28));
-      ColorSequenceKeypoint.new(3, Color3.fromRGB(207,15,43));
-      ColorSequenceKeypoint.new(4, Color3.fromRGB(176,28,171));
-      ColorSequenceKeypoint.new(5, Color3.fromRGB(0,161,222));
+ColorSequenceKeypoint.new(0, Color3.fromRGB(117,189,33));
+ColorSequenceKeypoint.new(1, Color3.fromRGB(255,199,40));
+ColorSequenceKeypoint.new(2, Color3.fromRGB(255,102,28));
+ColorSequenceKeypoint.new(3, Color3.fromRGB(207,15,43));
+ColorSequenceKeypoint.new(4, Color3.fromRGB(176,28,171));
+ColorSequenceKeypoint.new(5, Color3.fromRGB(0,161,222));
 }))
 Player.CharacterAdded:Connect(function(Character)
     for i = 1, 10 do
