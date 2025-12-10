f7944483da8bd04594b6f522b26564ccf12a8a89
min20111005 <min20111005@iCloud.com>
2025-12-10 21:14:38 +0900

    Refactor getRandomRGB for A and B teams

--- Games/Fling Things and People.lua
+++ Games/Fling Things and People.lua
@@ -1,12 +1,20 @@
+--A TEAM
 local function getRandomRGB()
-    local r = math.random(0, 255)
-    local g = math.random(0, 255)
-    local b = math.random(0, 255)
-    return { R = r, G = g, B = b }
+    local Ar = math.random(0, 255)
+    local Ag = math.random(0, 255)
+    local Ab = math.random(0, 255)
+    return { AR = Ar, AG = Ag, B = Ab }
 end
 
-local Acolor = getRandomRGB()
-local Bcolor = getRandomRGB()
+--B TEAM
+local function getRandomRGB()
+    local Br = math.random(0, 255)
+    local Bg = math.random(0, 255)
+    local Bb = math.random(0, 255)
+    return { BR = Br, BG = Bg, BB = Bb }
+end
+
+local color = getRandomRGB()
 local CONFIGURATION = {
     RANGE = 50
 }
@@ -15,9 +23,10 @@ local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
+
 ReplicatedStorage.DataEvents.UpdateLineColorsEvent:FireServer(ColorSequence.new({
-    ColorSequenceKeypoint.new(0, Color3.fromRGB(Acolor.R, Acolor.G, Acolor.B));
-    ColorSequenceKeypoint.new(1, Color3.fromRGB(Bcolor.R, Bcolor.G, Bcolor.B))
+    ColorSequenceKeypoint.new(0, Color3.fromRGB(color.AR, color.AG, color.AB));
+    ColorSequenceKeypoint.new(1, Color3.fromRGB(color.BR, color.BG, color.BB))
 }))
 Player.CharacterAdded:Connect(function(Character)
     for i = 1, 10 do
